4aa870de3249a7bcbfc5bf08a5ab909cb896fc96
Ryan-Fue <ryanfue@ucla.edu>
2024-12-29 18:37:53 -0800

    Added SQL query for changing datatype of trip_id

--- table_clean.sql
+++ table_clean.sql
@@ -5,3 +5,10 @@ WHERE tripduration < 0;
 
 DELETE FROM `cyclisticbikeshare-0._scripta531461b322daa1761c491ad5f61b363b6d8b564.AlteredDivvyTrips2020`
 WHERE tripduration < 0;
+
+--Changes data type in AlteredDivvyTrips2019 to match AlteredDivvyTrips 2020
+
+CREATE TABLE TripData.AlteredDivvyTrips2019V2 AS --Creates new table to contain altered data type
+SELECT * EXCEPT(trip_id),
+  CAST(trip_id AS STRING) AS trip_id --Changes trip_id from INT64 to STRING
+FROM `cyclisticbikeshare-0._scripta531461b322daa1761c491ad5f61b363b6d8b564.AlteredDivvyTrips2019`;
